167f18bd5042d775b134ef053b903cc65fc46835
ProgressAce <daiyaansallie7@gmail.com>
2024-03-13 10:01:09 +0200

    doc: document module's functions

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,9 +4,18 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 module.exports = {
+  /**
+   * Sends json response of the status of the database and redis connection.
+   * @param {Response} res a http response from express route
+   */
   getStatus: (res) => {
     res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   },
+
+  /**
+   * Sends json response of the total number of users and files in the database.
+   * @param {Response} res a http response from express route
+   */
   getStats: (res) => {
     (async () => {
       const userNb = await dbClient.nbUsers();
