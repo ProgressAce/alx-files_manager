62cf0e768e21f13295527c7404d70603317d5828
ProgressAce <daiyaansallie7@gmail.com>
2024-08-01 15:19:22 +0200

    feat: form mongodb client connect and interaction
    DBClient class is defined to handle the Mongo database connection and
    interaction with it. An instantiation of the class is exported for use
    by other modules. Environment variables are used to populate the host,
    port and database name for the database client connection.
    isAlive method is used to determine if the client connection is active.
    The nbUsers and nbFiles methods give the total number of documents for their
    respective 'users' and 'files' collection in the database.

---
+++ utils/db.js
@@ -0,0 +1,85 @@
+// Forms the MongoDB client connection and interaction.
+const MongoClient = require('mongodb/lib/mongo_client');
+
+/**
+ * Represents the database connection.
+ */
+class DBClient {
+    /**
+     * Instantiates a database client.
+     * @param {string} host the database host
+     * @param {string} port the database port
+     * @param {string} dbName the name of the database
+     */
+  constructor(host, port, dbName) {
+    const url = `mongodb://${host}:${port}/${dbName}`;
+
+    /**
+     * @type {import('mongodb/lib/collection')} The users collection.
+     */
+    this.users = null;
+
+    /**
+     * @type {import('mongodb/lib/collection')} The files collection.
+     */
+    this.files = null;
+
+    /**
+     * Connects to the MongoDB server and initializes the client and database.
+     */
+    (async () => {
+      try {
+        this.client = await MongoClient.connect(url, { useUnifiedTopology: true });
+        this.db = this.client.db(dbName);
+        
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      } catch (error) {
+        console.log('Error creating database client:', error);
+      }
+    })();
+  }
+
+  /**
+   * Determines if the connection is active.
+   * @returns True, if the database client is connected successfully
+   * otherwise, `false`.
+   */
+  isAlive() {
+    return Boolean(this.client);
+  }
+
+  /**
+   * Gives the number of documents in the users collection.
+   * @returns number of users.
+   */
+  async nbUsers() {
+    try {
+      return await this.users.countDocuments();
+    } catch (error) {
+      console.log('nbUsers countDocuments error:', error);
+      return null;
+    }
+  }
+
+  /**
+   * Gives the number of documents in the files collection.
+   * @returns number of files.
+   */
+  async nbFiles() {
+    try {
+      return await this.files.countDocuments();
+    } catch (error) {
+      console.log('nbFiles countDocuments error:', error);
+      return null;
+    }
+  }
+}
+
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || '27017';
+const dbName = process.env.DB_DATABASE || 'files_manager';
+
+const dbClient = new DBClient(host, port, dbName);
+
+module.exports = dbClient;
